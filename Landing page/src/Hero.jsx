9bf367e64f0a4214b7d53b21cc0a6193285a1ba9
VVSD Charan <vvsdcharan2001@gmail.com>
2023-06-18 19:54:42 +0530

    new file:   Landing page/.gitignore 	new file:   Landing page/README.md 	new file:   Landing page/index.html 	new file:   Landing page/package-lock.json 	new file:   Landing page/package.json 	new file:   Landing page/public/vite.svg 	new file:   Landing page/src/App.jsx 	new file:   Landing page/src/Context.jsx 	new file:   Landing page/src/Hero.jsx 	new file:   Landing page/src/NavLinks.jsx 	new file:   Landing page/src/Navbar.jsx 	new file:   Landing page/src/Sidebar.jsx 	new file:   Landing page/src/Submenu.jsx 	new file:   Landing page/src/assets/react.svg 	new file:   Landing page/src/data.jsx 	new file:   Landing page/src/index.css 	new file:   Landing page/src/main.jsx 	new file:   Landing page/vite.config.js

---
+++ Landing page/src/Hero.jsx
@@ -0,0 +1,15 @@
+const Hero = () => {
+  return (
+    <div className='hero-container'>
+      <div className='hero-center'>
+        <h1>
+          Welcome to <br /> Landing page
+        </h1>
+        <p>
+          This landing page can be used as a reference instead of coding from scratch
+        </p>
+      </div>
+    </div>
+  );
+};
+export default Hero;
