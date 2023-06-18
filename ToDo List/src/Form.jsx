c9d3fc7ca3116c6582c8a851f3edbd092c4b51e0
VVSD Charan <vvsdcharan2001@gmail.com>
2023-06-18 11:54:59 +0530

    modified:   ToDo List/index.html 	modified:   ToDo List/package-lock.json 	modified:   ToDo List/package.json 	modified:   ToDo List/src/App.jsx 	modified:   ToDo List/src/Form.jsx 	modified:   ToDo List/src/Items.jsx 	modified:   ToDo List/src/SingleItem.jsx 	modified:   ToDo List/src/main.jsx

--- ToDo List/src/Form.jsx
+++ ToDo List/src/Form.jsx
@@ -1,36 +1,34 @@
-import { useState } from "react";
+import { useState } from 'react';
 import { toast } from 'react-toastify';
 
-export const Form = ({addItem}) =>
-{
-    const [newItemName,setNewItemName]=useState('');
+const Form = ({ addItem }) => {
+  const [newItemName, setNewItemName] = useState('');
 
-    const handleSubmit = (e) =>
-    {
-        e.preventDefault();
-       
-        if(!newItemName)
-        {
-            toast.error("Please provide a task");
-            return;
-        }
-
-        addItem(newItemName);
-        setNewItemName('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!newItemName) {
+      toast.error('please provide value');
+      return;
     }
+    addItem(newItemName);
+    setNewItemName('');
+  };
 
-    return (
-        <form onSubmit={handleSubmit}>
-
-            <h4>To Do List</h4>
-
-            <div className="form-control">
-
-                <input type="text" name="newItemName" className="form-input" value={newItemName} onChange={(e)=>setNewItemName(e.target.value)}/>
-                <button className="btn" type="submit">Add Task</button>
-
-            </div>
-        </form>
-    );
-
+  return (
+    <form onSubmit={handleSubmit}>
+      <h4>To Do List</h4>
+      <div className='form-control'>
+        <input
+          type='text '
+          className='form-input'
+          value={newItemName}
+          onChange={(event) => setNewItemName(event.target.value)}
+        />
+        <button type='submit' className='btn'>
+          add item
+        </button>
+      </div>
+    </form>
+  );
 };
+export default Form;
--- ToDo List/src/Items.jsx
+++ ToDo List/src/Items.jsx
@@ -1,15 +1,19 @@
-import { SingleItem } from "./SingleItem";
+import SingleItem from './SingleItem';
 
-export const Items = ({items,removeItem,editItem}) =>
-{
-    return (
-        <div className="items">
-            {
-                items.map((item)=>
-                {
-                    return <SingleItem key={item.id} item={item} removeItem={removeItem} editItem={editItem}/>
-                })
-            }
-        </div>
-    );
+const Items = ({ items, removeItem, editItem }) => {
+  return (
+    <div className='items'>
+      {items.map((item) => {
+        return (
+          <SingleItem
+            key={item.id}
+            item={item}
+            removeItem={removeItem}
+            editItem={editItem}
+          />
+        );
+      })}
+    </div>
+  );
 };
+export default Items;
